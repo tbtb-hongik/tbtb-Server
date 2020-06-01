c79a96eba1d37ca20b548596424bfb190b323975
ParkSolMin <50397888+ParkSolMin@users.noreply.github.com>
2020-06-01 22:33:35 +0900

    iOS socket init

--- src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
+++ src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
@@ -10,33 +10,53 @@ import java.net.Socket;
 
 @RestController
 public class TBController implements Runnable {
-    public static final int ServerPort = 5555;
+    public static final int androidServerPort = 5555;
+    public static final int iosServerPort = 6666;
     String str;
 
     @Override
     public void run() {
         try {
-            ServerSocket serverSocket = new ServerSocket(ServerPort);
+            ServerSocket androidServerSocket = new ServerSocket(androidServerPort);
+            ServerSocket iosServerSocket = new ServerSocket(iosServerPort);
             System.out.println("S: Connecting...");
 
             System.out.println(InetAddress.getLocalHost().getHostAddress());
             while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("S: Receiving...");
+                Socket androidSocket = androidServerSocket.accept();
+                System.out.println("Android S: Receiving...");
                 try {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedReader in = new BufferedReader(new InputStreamReader(androidSocket.getInputStream()));
                     str = in.readLine();
 
-                    System.out.println("S: Received: '" + str + "'");
+                    System.out.println("Android S: Received: '" + str + "'");
 
-                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                    out.println("Server Received " + str);
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(androidSocket.getOutputStream())), true);
+                    out.println("Android Server Received " + str);
                 } catch (Exception e) {
-                    System.out.println("S: Error");
+                    System.out.println("Android S: Error");
                     e.printStackTrace();
                 } finally {
-                    socket.close();
-                    System.out.println("S: Done.");
+                    androidSocket.close();
+                    System.out.println("Android S: Done.");
+                }
+
+                Socket iosSocket = iosServerSocket.accept();
+                System.out.println("iOS S: Receiving...");
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(androidSocket.getInputStream()));
+                    str = in.readLine();
+
+                    System.out.println("iOS S: Received: '" + str + "'");
+
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(androidSocket.getOutputStream())), true);
+                    out.println("iOS Server Received " + str);
+                } catch (Exception e) {
+                    System.out.println("iOS S: Error");
+                    e.printStackTrace();
+                } finally {
+                    androidSocket.close();
+                    System.out.println("iOS S: Done.");
                 }
             }
         } catch (Exception e) {
