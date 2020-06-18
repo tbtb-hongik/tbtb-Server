011c00acccc4abff242a53c84c785da202987c9c
ParkSolMin <50397888+ParkSolMin@users.noreply.github.com>
2020-06-18 19:10:43 +0900

    HashMap으로 request 받기

--- src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
+++ src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
@@ -3,15 +3,19 @@ package io.psol.tbtb.tbtb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @Controller
 public class TBController {
     @RequestMapping(value = "/android", method = RequestMethod.POST)
-    public @ResponseBody String test(@RequestParam("url") String url){
+    public @ResponseBody String test(@RequestParam("url") HashMap<String, String> url){
         // url 은 받은 데이터
-        System.out.println("\n/android Requested URL : \n" + url);
+        String imgUrl = url.get(url.keySet());
+        System.out.println(url + "\n/android Requested URL : \n" + imgUrl);
 
         // AI api 처리된 데이터
-        String TTS = "가공가공>_<";
+        String TTS = "초절정 귀요미 말티즈 민이, Cute cute 별이";
+        
         return TTS;
     }
 }
