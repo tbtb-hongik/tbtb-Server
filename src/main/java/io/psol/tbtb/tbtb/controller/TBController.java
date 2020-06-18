2f835e02dd1996c1047cb20c8972c344d66dbd62
ParkSolMin <50397888+ParkSolMin@users.noreply.github.com>
2020-06-18 19:32:14 +0900

    hashmap 폐기 후 os 파라미터로 변경

--- src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
+++ src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
@@ -3,19 +3,17 @@ package io.psol.tbtb.tbtb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-
 @Controller
 public class TBController {
     @RequestMapping(value = "/android", method = RequestMethod.POST)
-    public @ResponseBody String test(@RequestParam("url") HashMap<String, String> url){
+    public @ResponseBody
+    String test(@RequestParam("url") String url, @RequestParam("os") String os) {
         // url 은 받은 데이터
-        String imgUrl = url.get(url.keySet());
-        System.out.println(url + "\n/android Requested URL : \n" + imgUrl);
+        System.out.println(os + " URL : \n" + url);
 
         // AI api 처리된 데이터
-        String TTS = "초절정 귀요미 말티즈 민이, Cute cute 별이";
-        
+        String TTS = "초절정 귀요미 민이, Cute cute 별이";
+
         return TTS;
     }
 }
