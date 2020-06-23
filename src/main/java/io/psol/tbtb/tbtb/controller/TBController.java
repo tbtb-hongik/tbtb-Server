61ffa399a45c39550b03a6b5abe13c96613cf610
ParkSolMin <50397888+ParkSolMin@users.noreply.github.com>
2020-06-23 16:46:01 +0900

    Mod TTS변경

--- src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
+++ src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
@@ -17,7 +17,7 @@ public class TBController {
         System.out.println(os + " URL : \n" + url);
 
         // AI api 처리된 데이터
-        String TTS = "베이지색 운동화";
+        String TTS = "안녕하세요! 스니커즈빌딩입니다. 스니커즈빌딩은 2020년 3월 개업한 소규모 슈즈 전문 스토어 입니다. 판매하는 모든 제품은 100% 정품임을 약속드리며 좋은 물건 합리적인 가격에 제공해드리는 스니커즈빌딩이 되도록 노력하겠습니다. 감사합니다.";
 
         // DB 저장
         TBModel image = new TBModel();
@@ -35,7 +35,7 @@ public class TBController {
         System.out.println(os + " URL : \n" + url);
 
         // AI api 처리된 데이터
-        String TTS = "초절정 귀요미 민이, Cute cute 별이";
+        String TTS = "iOS";
 
         // DB 저장
         TBModel image = new TBModel();
