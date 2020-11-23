7700bbdefd13c545a25aa99f64219284bdcf240d
ParkSolMin <50397888+ParkSolMin@users.noreply.github.com>
2020-11-23 19:44:45 +0900

    Update vision api test

--- src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
+++ src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
@@ -6,6 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.protobuf.ByteString;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class TBController {
     @Autowired
@@ -15,6 +28,44 @@ public class TBController {
     public @ResponseBody String test(@RequestParam("url") String url, @RequestParam("os") String os) {
         // url 은 받은 데이터
         System.out.println(os + " URL : \n" + url);
+
+        try {
+
+            String imageFilePath = ""; //여기 설정해줘야함(test이미지 경로)
+
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+
+            ByteString imgBytes = ByteString.readFrom(new FileInputStream(imageFilePath));
+
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+            requests.add(request);
+
+            try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+                BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+                List<AnnotateImageResponse> responses = response.getResponsesList();
+
+                for (AnnotateImageResponse res : responses) {
+                    if (res.hasError()) {
+                        System.out.printf("Error: %s\n", res.getError().getMessage());
+                    }
+
+                    System.out.println("Text : ");
+                    System.out.println(res.getTextAnnotationsList().get(0).getDescription());
+
+                    // For full list of available annotations, see http://g.co/cloud/vision/docs
+			    	/*for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+
+						//System.out.printf("Text: %s\n", annotation.getDescription());
+						//System.out.printf("Position : %s\n", annotation.getBoundingPoly());
+					}*/
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
 
         // AI api 처리된 데이터
         String TTS = "안녕하세요! 스니커즈빌딩입니다. 스니커즈빌딩은 2020년 3월 개업한 소규모 슈즈 전문 스토어 입니다. 판매하는 모든 제품은 100% 정품임을 약속드리며 좋은 물건 합리적인 가격에 제공해드리는 스니커즈빌딩이 되도록 노력하겠습니다. 감사합니다.";
