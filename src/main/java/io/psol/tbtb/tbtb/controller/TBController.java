3bd7b55b1d22f016cbb0cd441def43a7d355a117
ParkSolMin <50397888+ParkSolMin@users.noreply.github.com>
2020-11-24 13:24:40 +0900

    Update vision api

--- src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
+++ src/main/java/io/psol/tbtb/tbtb/controller/TBController.java
@@ -1,16 +1,13 @@
  package io.psol.tbtb.tbtb.controller;
 
-import com.google.cloud.vision.v1.*;
 import io.psol.tbtb.tbtb.model.TBModel;
 import io.psol.tbtb.tbtb.service.TBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import com.google.cloud.vision.v1.Feature.Type;
-import com.google.protobuf.ByteString;
+import com.google.cloud.vision.v1.*;
 
-import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,42 +21,42 @@ public class TBController {
         // url 은 받은 데이터
         System.out.println(os + " URL : \n" + url);
 
-        try {
-            List<AnnotateImageRequest> requests = new ArrayList<>();
-
-            ImageSource imgUri = ImageSource.newBuilder().setImageUri(url).build();
-            Image img = Image.newBuilder().setSource(imgUri).build();
-
-            Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
-            AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
-            requests.add(request);
-
-            try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
-                BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
-                List<AnnotateImageResponse> responses = response.getResponsesList();
-                System.out.printf("test: %s\n", requests);
-                System.out.printf("test: %s\n", response);
-                System.out.printf("test: %s\n", responses);
-
-                for (AnnotateImageResponse res : responses) {
-                    if (res.hasError()) {
-                        System.out.printf("Error: %s\n", res.getError().getMessage());
-                    }
-
-                    System.out.println("Text : ");
-                    System.out.println(res.getTextAnnotationsList().get(0).getDescription());
-
-                    // For full list of available annotations, see http://g.co/cloud/vision/docs
-			    	/*for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-
-						//System.out.printf("Text: %s\n", annotation.getDescription());
-						//System.out.printf("Position : %s\n", annotation.getBoundingPoly());
-					}*/
-                }
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            ImageSource imgUri = ImageSource.newBuilder().setImageUri(url).build();
+//            Image img = Image.newBuilder().setSource(imgUri).build();
+//
+//            Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+//            AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+//
+//            List<AnnotateImageRequest> requests = new ArrayList<>();
+//            requests.add(request);
+//
+//            try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+//                BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+//                List<AnnotateImageResponse> responses = response.getResponsesList();
+//                System.out.printf("test: %s\n", requests);
+//                System.out.printf("test: %s\n", response);
+//                System.out.printf("test: %s\n", responses);
+//
+//                for (AnnotateImageResponse res : responses) {
+//                    if (res.hasError()) {
+//                        System.out.printf("Error: %s\n", res.getError().getMessage());
+//                    }
+//
+//                    System.out.println("Text : ");
+//                    System.out.println(res.getTextAnnotationsList().get(0).getDescription());
+//
+//                    // For full list of available annotations, see http://g.co/cloud/vision/docs
+//			    	/*for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+//
+//						//System.out.printf("Text: %s\n", annotation.getDescription());
+//						//System.out.printf("Position : %s\n", annotation.getBoundingPoly());
+//					}*/
+//                }
+//            }
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
 
         // AI api 처리된 데이터
         String TTS = "안녕하세요! 스니커즈빌딩입니다. 스니커즈빌딩은 2020년 3월 개업한 소규모 슈즈 전문 스토어 입니다. 판매하는 모든 제품은 100% 정품임을 약속드리며 좋은 물건 합리적인 가격에 제공해드리는 스니커즈빌딩이 되도록 노력하겠습니다. 감사합니다.";
@@ -90,5 +87,51 @@ public class TBController {
 //        tbService.insert(image);
 
         return TTS;
+    }
+
+    public void analysisImage(String url) {
+        ImageSource imgUri = ImageSource.newBuilder().setImageUri(url).build();
+        Image img = Image.newBuilder().setSource(imgUri).build();
+
+        Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        requests.add(request);
+
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            System.out.println("Text : ");
+            System.out.printf("requests: %s\n", requests);
+            System.out.printf("response: %s\n", response);
+            System.out.printf("responses: %s\n", responses);
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                }
+
+                List<EntityAnnotation> annotations = res.getLabelAnnotationsList();
+                System.out.println("for문 : ");
+                for (EntityAnnotation annotation : annotations){
+                    System.out.println(annotation.getDescription());
+                    System.out.println(annotation.getLocale());
+                    System.out.println(annotation.getMid());
+                    System.out.println(annotation.toString());
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+			    	/*for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+
+						//System.out.printf("Text: %s\n", annotation.getDescription());
+						//System.out.printf("Position : %s\n", annotation.getBoundingPoly());
+					}*/
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
